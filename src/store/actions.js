13881e2ed59969d666d8e737cdb4612398f4ee0b
Israel Ramirez <israplenitud@gmail.com>
2019-10-10 17:38:56 -0500

    Redirigiendo usuarios  a ChatDashboard

--- src/store/actions.js
+++ src/store/actions.js
@@ -17,8 +17,24 @@ export default {
                 username: currentUser.id,
                 name: currentUser.name
             });
+            // Save list of user's rooms in store
+            const rooms = currentUser.rooms.map(room => ({
+                id: room.id,
+                name: room.name
+            }))
+            commit('setRooms', rooms);
+
+            // Subscribe user to a room
+            const activeRoom = state.activeRoom || rooms[0]; // pick last used room, or the first one
+            commit('setActiveRoom', {
+                id: activeRoom.id,
+                name: activeRoom.name
+            });
+            await chatkit.subscribeToRoom(activeRoom.id);
+
             commit('setReconnect', false);
 
+            return true;
             // Test state.user
             console.log(state.user);
         } catch (error) {
